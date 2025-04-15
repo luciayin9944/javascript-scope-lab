66478216ffc3884550414e7ce9167a7c0c89d1e8
luciayin9944 <luxiayin94@gmail.com>
2025-04-15 13:16:21 -0700

    update index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 // Write your solution in this file!
 
+
 // Global scope variables
 const burgers = ['Hamburger', 'Cheeseburger'];
 let featuredDrink = 'Strawberry Milkshake';
 
 // Function: addBurger
 function addBurger() {
-    // function-scoped variable
-  const newBurger = 'Flatburger'; 
+  // function-scoped variable
+  const newBurger = 'Flatburger';
   burgers.push(newBurger);
 }
 
-// Always-true conditional block
-if (true) {
-    // block-scoped variable
-  const anotherNewBurger = 'Maple Bacon Burger'; 
+if(true){
+  // block-scoped variable
+  const anotherNewBurger = 'Maple Bacon Burger';
   burgers.push(anotherNewBurger);
 }
 
@@ -22,3 +22,5 @@ if (true) {
 function changeFeaturedDrink() {
   featuredDrink = 'The JavaShake';
 }
+
+
