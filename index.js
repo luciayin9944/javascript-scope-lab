0a04dd78d205c84322b56ea32e4199b217a5e966
luciayin9944 <luxiayin94@gmail.com>
2025-04-15 13:03:42 -0700

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution in this file!
+
+// Global scope variables
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// Function: addBurger
+function addBurger() {
+    // function-scoped variable
+  const newBurger = 'Flatburger'; 
+  burgers.push(newBurger);
+}
+
+// Always-true conditional block
+if (true) {
+    // block-scoped variable
+  const anotherNewBurger = 'Maple Bacon Burger'; 
+  burgers.push(anotherNewBurger);
+}
+
+// Function: changeFeaturedDrink
+function changeFeaturedDrink() {
+  featuredDrink = 'The JavaShake';
+}
+
+
+
+// my original code
+
+// const burgers = ["Hamburger", "Cheeseburger"];
+// const featuredDrink = "Strawberry Milkshake"; // 用 let 而不是 const
+
+// function addBurger() {
+//     const newBurger = "Flatburger";
+//     burgers.push(newBurger);
+
+//     if (order === "othernewBurger") {
+//         const othernewBurger = "Maple Bacon Burger";
+//         burgers.push(othernewBurger);
+//     };
+// }
+
+// function changeFeaturedDrink() {
+//     featuredDrink = "The JavaShake";
+// };
